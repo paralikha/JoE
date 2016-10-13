b0f25efb49e3a74adbc44f1c615261e7e2e6d8a8
User <User>
2016-10-13 12:05:20 +0800

    carousel

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,20 +1,3 @@
-// News & Event Gallery
-$(document).ready(function() {
-  function random(owlSelector){
-    owlSelector.children().sort(function(){
-        return Math.round(Math.random()) - 0.5;
-    }).each(function(){
-        $(this).appendTo(owlSelector);
-    });
-}
-    $("#owl-demo").owlCarousel({
-        navigation: false,
-        beforeInit : function(elem){
-            random(elem);
-        }
-    });
-});
-
 // Nav Dropdown
 $('ul.nav li.dropdown').hover(function() {
     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
@@ -30,11 +13,20 @@ $(document).ready(
     }
 );
 
-// Animated Slide (Home)
-$(document).ready(function() {  
-    $("#animated_slide").owlCarousel({
-        navigation : false,
-        singleItem:true,
-        autoPlay: 5000,
-    });
+// lightSlider
+$(document).ready(function() {
+    $('#imageGallery').lightSlider({
+        gallery:true,
+        item:3,
+        loop:true,
+        thumbItem:9,
+        slideMargin:0,
+        enableDrag: false,
+        currentPagerPosition:'left',
+        onSliderLoad: function(el) {
+            el.lightGallery({
+                selector: '#imageGallery .lslide'
+            });
+        }   
+    });  
 });
