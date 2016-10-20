05f05bea9dd47796dc5c553f92dad256e5b3cc3c
User <User>
2016-10-20 10:32:56 +0800

    revisions

--- resources/js/test.js
+++ resources/js/test.js
@@ -38,7 +38,52 @@ function rocket_animate_bottomLeft() {
 		else {
 			$waypoint.removeClass('in-view');
 		}
+
+		else {
+			$element.removeClass('in-view');
+		}
 	})
+}
 
+// waypoint
+var $animation_elements = $('.animation-element');
+var $window = $(window);
 
+$window.on('scroll', check_waypoint);
+$window.on('scroll resize on', check_waypoint_view);
+$window.trigger('scroll');
+
+function waypoint_op() {
+	var window_height = $window.height();
+	var window_top_position = $window.scrollTop();
+	var window_bottom_position = (window_top_position) + window_height);
+
+$each($animation_elements, function() {
+	var $element = $(this);
+	var element_height = $element.outerHeight();
+	var element_top_position = $element.offset().top;
+	var_element_bottom_position = (element_top_position + element_height);
+
+	if ((element_bottom_position >= window_top_position) &&
+		(element_top_position <= window_top_position)) {
+		$waypoint.addClass('in-view');
+	}
+	else {
+		$waypoint.removeClass('in-view');
+	}
+	else {
+		$element.removeClass('in-view');
+	}
+});
+
+$foreach($animation_waypoint, function() {
+	var $element = $(this);
+	var element_height = $element.outerHeight();
+	var element_top_position = $element.offset().top;
+	var _element_bottom_position = (element_top_position)
+}
+
+	if (element_top_position ))
+
+})
 }
